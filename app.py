3073e49fee6a91cfd8ac0513ba9e5f2a1094857e
Akash Singh <akashsingh1@hotmail.com>
2023-09-26 18:30:09 +0530

    install wkhtmltopdf

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, send_file
+import pdfkit
+
+app = Flask(__name__)
+
+# Configure pdfkit with the path to the wkhtmltopdf executable
+config = pdfkit.configuration(wkhtmltopdf="/usr/local/bin/wkhtmltopdf")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/generate-pdf", methods=["POST"])
+def generate_pdf():
+    html_content = request.form.get("html_content")
+
+    # Path to save the generated PDF
+    pdf_output_path = "documents/output.pdf"
+
+    # Generate the PDF from HTML content
+    pdfkit.from_string(html_content, pdf_output_path, configuration=config)
+
+    return send_file(pdf_output_path, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
